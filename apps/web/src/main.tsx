f0ce088aab231c76b58dd61402e0495f71408f91
Akinbola247 <akinbolakehinde247@gmail.com>
2026-06-14 18:43:49 +0100

    Bootstrap React app with router and wallet provider

---
+++ apps/web/src/main.tsx
@@ -0,0 +1,21 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Provider as JotaiProvider } from "jotai";
+import { Buffer } from "buffer";
+
+import { App } from "./App";
+import { WalletProvider } from "./components/WalletProvider";
+import "./styles/global.css";
+import "./styles/pages.css";
+
+(globalThis as unknown as { Buffer: typeof Buffer }).Buffer = Buffer;
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <JotaiProvider>
+      <WalletProvider>
+        <App />
+      </WalletProvider>
+    </JotaiProvider>
+  </StrictMode>
+);
