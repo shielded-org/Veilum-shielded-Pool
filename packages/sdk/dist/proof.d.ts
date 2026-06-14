1700c7208680cd42a2647a0a0a73801180563897
Akinbola247 <akinbolakehinde247@gmail.com>
2026-06-14 18:43:54 +0100

    Add compiled SDK type declarations for proof

---
+++ packages/sdk/dist/proof.d.ts
@@ -0,0 +1,31 @@
+import type { Hex32 } from "./types.js";
+export type ProofInputs = {
+    /** Decimal string field element (matches nargo Prover.toml). */
+    spending_key: string;
+    in_amounts: [string, string];
+    in_blindings: [Hex32, Hex32];
+    merkle_siblings: [Hex32[], Hex32[]];
+    merkle_directions: [boolean[], boolean[]];
+    out_amounts: [string, string];
+    out_recipient_pks: [Hex32, Hex32];
+    out_blindings: [Hex32, Hex32];
+    token: Hex32;
+    merkle_root: Hex32;
+    nullifiers: [Hex32, Hex32];
+    out_commitments: [Hex32, Hex32];
+    fee: string;
+    fee_recipient_pk: Hex32;
+    mode: string;
+    unshield_recipient: Hex32;
+    unshield_amount: string;
+    unshield_token_address: Hex32;
+};
+export declare function loadCircuitArtifact(circuitsDir?: string): {
+    bytecode: string;
+    abi: unknown;
+};
+export declare function generateUltraHonkProof(inputs: ProofInputs, circuitsDir?: string): Promise<{
+    proofBytes: Buffer;
+    publicInputsBytes: Buffer;
+}>;
+export declare function stripPublicInputsFromProof(proofWithPublicInputs: Buffer): Buffer;
