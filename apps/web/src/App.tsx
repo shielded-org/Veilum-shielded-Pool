f5551f8804d28fef9ae5c27cfdb38c1e89e98119
Akinbola247 <akinbolakehinde247@gmail.com>
2026-06-27 15:20:11 +0100

    Use theme-aware toast container in app shell

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -3,11 +3,12 @@ import { ToastContainer } from "react-toastify";
 
 import { DashboardLayout } from "./components/DashboardLayout";
 import { LandingLayout } from "./components/LandingLayout";
+import { useTheme } from "./hooks/use-theme";
+import { WalletConnectionProvider } from "./hooks/use-wallet-connection";
 import { AspAdminPage } from "./pages/AspAdminPage";
 import { AboutPage } from "./pages/AboutPage";
 import { DashboardHome } from "./pages/DashboardHome";
 import { FaucetPage } from "./pages/FaucetPage";
-import { HowToUsePage } from "./pages/HowToUsePage";
 import { KeysPage } from "./pages/KeysPage";
 import { LandingPage } from "./pages/LandingPage";
 import { NotesPage } from "./pages/NotesPage";
@@ -15,9 +16,11 @@ import { ShieldPage } from "./pages/ShieldPage";
 import { TransferPage } from "./pages/TransferPage";
 import { UnshieldPage } from "./pages/UnshieldPage";
 
-export function App() {
+function AppRoutes() {
+  const { theme } = useTheme();
+
   return (
-    <BrowserRouter>
+    <>
       <ToastContainer
         position="bottom-right"
         autoClose={8000}
@@ -25,7 +28,7 @@ export function App() {
         closeOnClick
         pauseOnFocusLoss
         draggable
-        theme="dark"
+        theme={theme}
         hideProgressBar={false}
         icon={false}
         toastClassName="veilum-toast"
@@ -36,7 +39,7 @@ export function App() {
         <Route element={<LandingLayout />}>
           <Route index element={<LandingPage />} />
           <Route path="about" element={<AboutPage />} />
-          <Route path="how-to-use" element={<HowToUsePage />} />
+          <Route path="how-to-use" element={<Navigate to="/about#getting-started" replace />} />
         </Route>
         <Route path="dashboard" element={<DashboardLayout />}>
           <Route index element={<DashboardHome />} />
@@ -51,6 +54,16 @@ export function App() {
         </Route>
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
+    </>
+  );
+}
+
+export function App() {
+  return (
+    <BrowserRouter>
+      <WalletConnectionProvider>
+        <AppRoutes />
+      </WalletConnectionProvider>
     </BrowserRouter>
   );
 }
