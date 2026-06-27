65ee0818c2c07ea84d66682a9576a80602a23f06
Akinbola247 <akinbolakehinde247@gmail.com>
2026-06-27 15:20:11 +0100

    Wire ThemeProvider and new global style imports

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -4,10 +4,16 @@ import { Provider as JotaiProvider } from "jotai";
 import { Buffer } from "buffer";
 
 import { App } from "./App";
+import { ThemeProvider } from "./hooks/use-theme";
 import { WalletProvider } from "./components/WalletProvider";
 import "./styles/global.css";
 import "./styles/pages.css";
 import "./styles/impeccable-theme.css";
+import "./styles/landing-tailwind.css";
+import "./styles/wallet-kit.css";
+import "./styles/onboarding-dialog.css";
+import "./styles/theme-toggle.css";
+import "./styles/dashboard-light.css";
 import "./styles/mobile.css";
 import "react-toastify/dist/ReactToastify.css";
 import "./styles/toast.css";
@@ -17,9 +23,11 @@ import "./styles/toast.css";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <JotaiProvider>
-      <WalletProvider>
-        <App />
-      </WalletProvider>
+      <ThemeProvider>
+        <WalletProvider>
+          <App />
+        </WalletProvider>
+      </ThemeProvider>
     </JotaiProvider>
   </StrictMode>
 );
